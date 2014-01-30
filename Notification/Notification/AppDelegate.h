5985125251d474ecdd833c1628c3b16676cf4edf
forcha <arielcardieri@gmail.com>
2014-01-30 15:58:04 +1300

    Notification idea used in a lot of new iOS 7 apps.

---
+++ Notification/Notification/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Notification
+//
+//  Created by Ariel Cardieri on 30/01/14.
+//  Copyright (c) 2014 Forcha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
